3e0197f70c3c1016449c291fbfa1f524546f1b33
Marvin Drude <marvin@gidd.io>
2025-07-20 14:20:32 +0200

    Added type serializers

---
+++ CodeAnalytics.Engine/Serialization/Components/Types/ClassSerializer.cs
@@ -0,0 +1,35 @@
+﻿using CodeAnalytics.Engine.Common.Buffers;
+using CodeAnalytics.Engine.Common.Buffers.Dynamic;
+using CodeAnalytics.Engine.Contracts.Components.Types;
+using CodeAnalytics.Engine.Contracts.Serialization;
+using CodeAnalytics.Engine.Serialization.Ids;
+
+namespace CodeAnalytics.Engine.Serialization.Components.Types;
+
+public sealed class ClassSerializer : ISerializer<ClassComponent>
+{
+   public static void Serialize(ref ByteWriter writer, ref ClassComponent ob)
+   {
+      NodeIdSerializer.Serialize(ref writer, ref ob.Id);
+      NodeIdSerializer.Serialize(ref writer, ref ob.BaseClassId);
+      
+      writer.WriteByte(ob.Flags.RawByte);
+   }
+
+   public static bool TryDeserialize(ref ByteReader reader, out ClassComponent ob)
+   {
+      ob = new ClassComponent();
+      if (!NodeIdSerializer.TryDeserialize(ref reader, out ob.Id))
+      {
+         return false;
+      }
+
+      if (!NodeIdSerializer.TryDeserialize(ref reader, out ob.BaseClassId))
+      {
+         return false;
+      }
+
+      ob.Flags = new PackedBools(reader.ReadByte());
+      return true;
+   }
+}
---
+++ CodeAnalytics.Engine/Serialization/Components/Types/InterfaceSerializer.cs
@@ -0,0 +1,25 @@
+﻿using CodeAnalytics.Engine.Common.Buffers;
+using CodeAnalytics.Engine.Contracts.Components.Types;
+using CodeAnalytics.Engine.Contracts.Serialization;
+using CodeAnalytics.Engine.Serialization.Ids;
+
+namespace CodeAnalytics.Engine.Serialization.Components.Types;
+
+public sealed class InterfaceSerializer : ISerializer<InterfaceComponent>
+{
+   public static void Serialize(ref ByteWriter writer, ref InterfaceComponent ob)
+   {
+      NodeIdSerializer.Serialize(ref writer, ref ob.Id);
+   }
+
+   public static bool TryDeserialize(ref ByteReader reader, out InterfaceComponent ob)
+   {
+      ob = new InterfaceComponent();
+      if (!NodeIdSerializer.TryDeserialize(ref reader, out ob.Id))
+      {
+         return false;
+      }
+      
+      return true;
+   }
+}

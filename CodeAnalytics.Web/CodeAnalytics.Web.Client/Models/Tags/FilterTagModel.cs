301701c0a2b078c2be74ecca8930fba9529714f3
Marvin Drude <marvin@gidd.io>
2025-07-26 13:24:27 +0200

    Created tag control for filtering in search

---
+++ CodeAnalytics.Web/CodeAnalytics.Web.Client/Models/Tags/FilterTagModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeAnalytics.Web.Client.Models.Tags;
+
+public sealed class FilterTagModel
+{
+   public required string Name { get; set; }
+   
+   public required string Value { get; set; }
+   
+   public bool IsActive { get; set; }
+   
+   public int Order { get; set; }
+}

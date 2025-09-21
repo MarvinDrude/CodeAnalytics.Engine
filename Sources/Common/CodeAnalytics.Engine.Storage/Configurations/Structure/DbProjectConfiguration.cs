3f9b3b4e33e7d2ff80e84fed2b337a586f4b9bbb
Marvin Drude <marvin@gidd.io>
2025-09-21 14:14:01 +0200

    Start adding new storage structure with ef core

---
+++ Sources/Common/CodeAnalytics.Engine.Storage/Configurations/Structure/DbProjectConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using CodeAnalytics.Engine.Storage.Models.Structure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace CodeAnalytics.Engine.Storage.Configurations.Structure;
+
+public sealed class DbProjectConfiguration : IEntityTypeConfiguration<DbProject>
+{
+   public void Configure(EntityTypeBuilder<DbProject> builder)
+   {
+      builder.HasKey(x => x.Id);
+      
+      builder.Property(x => x.Id)
+         .HasConversion(IdConverter)
+         .ValueGeneratedOnAdd();
+
+      builder.HasIndex(x => x.Name)
+         .IsUnique(false);
+      builder.Property(x => x.Name)
+         .HasMaxLength(2_000);
+
+      builder.HasIndex(x => x.RelativeFilePath)
+         .IsUnique();
+      builder.Property(x => x.RelativeFilePath)
+         .HasMaxLength(2_000);
+
+      builder.HasIndex(x => x.AssemblyName)
+         .IsUnique();
+      builder.Property(x => x.AssemblyName)
+         .HasMaxLength(2_000);
+
+      builder.HasMany(x => x.Solutions)
+         .WithMany(x => x.Projects);
+
+      builder.HasMany(x => x.ReferencedProjects)
+         .WithMany(x => x.ReferencedByProjects);
+   }
+
+   public static readonly ValueConverter<DbProjectId, long> IdConverter = 
+      new(id => id.Value, id => new DbProjectId(id));
+}
---
+++ Sources/Common/CodeAnalytics.Engine.Storage/Models/Structure/DbFile.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeAnalytics.Engine.Storage.Models.Structure;
+
+public sealed class DbFile
+{
+   public DbFileId Id { get; set; }
+   
+   public required string Name { get; set; }
+   
+   public required string RelativeFilePath { get; set; }
+
+   public List<DbProject> Projects { get; set; } = [];
+}
+
+public readonly record struct DbFileId(long Value)
+{
+   public static readonly DbFileId Empty = new(0);
+   
+   public static implicit operator long(DbFileId id) => id.Value;
+   public static implicit operator DbFileId(long value) => new(value);
+}
